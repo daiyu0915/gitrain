09cb502f2a145032a57bb8c980ce597307d28ad9
daiyu0915 <65677996@qq.com>
2020-08-10 10:13:34 +0800

    Updates

--- setupTests.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-require("@testing-library/jest-dom/extend-expect");
-
-var __signature__ = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal["default"].signature : function (a) {
-  return a;
-};
